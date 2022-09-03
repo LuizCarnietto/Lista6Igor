7e536eaf3530263540b5c7f5c4a55c214e70c8af
LuizCarnietto <101845917+LuizCarnietto@users.noreply.github.com>
2022-09-03 11:28:36 -0300

    Update ex6.java

--- Lista6/ex6.java
+++ Lista6/ex6.java
@@ -2,24 +2,23 @@ package lista6;
 
 import java.util.Scanner;
 
-public class ex6 {
+public class Ex3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] vet = new int[10];
+        Scanner in = new Scanner(System.in);
+        int vet [] = new int[10];
+        int i, j, igual = 0;
 
-        for (int i = 0; i < 10; i++){
-            System.out.println("Informe o valor da posicao "+i);
-            vet[i] = sc.nextInt();
+        for(i = 0; i < 10; i++) {
+            System.out.println("Digite o "+(i + 1)+" valor: ");
+            vet[i] = in.nextInt();
         }
 
-        System.out.println("Informe o valor X");
-        int x = sc.nextInt();
-
-
-        System.out.print("Os multiplos de X sao:");
-        for (int i = 0; i < 10; i++){
-            if (vet[i] % x == 0){
-                System.out.print(vet[i]+" ");
+        for(i = 0; i < 10; i++) {
+            for(j = 0; j < 9; j++) {
+                if(vet[i] == vet[j] && j!=i) {
+                    igual = vet[i];
+                    System.out.println("numeros iguais " + igual);
+                }
             }
         }
     }
